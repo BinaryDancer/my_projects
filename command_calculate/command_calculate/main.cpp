8caa6a4fa1f40b7755a2dd66d3784be825ed8dab
Pavel <pav.mamaew@yandex.ru>
2018-10-19 00:19:10 +0300

    new files

---
+++ command_calculate/command_calculate/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  command_calculate
+//
+//  Created by Мамаев Павел on 18/10/2018.
+//  Copyright © 2018 Мамаев Павел. All rights reserved.
+//
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
