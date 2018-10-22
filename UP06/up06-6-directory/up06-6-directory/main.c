0507d05ff83b8f204653176987476a521d78aa81
Pavel <pav.mamaew@yandex.ru>
2018-10-22 18:58:12 +0300

    global to local

--- UP06/up06-6-directory/up06-6-directory/main.c
+++ UP06/up06-6-directory/up06-6-directory/main.c
@@ -19,8 +19,7 @@
 int compare(const void * x1, const void * x2) {
     return (strcasecmp(*(char**)x1, *(char**)x2));
 }
-char path[PATH_MAX];
-int traverse(const char *dir, const char *name_dir, int len_dir)
+int traverse(const char *dir, const char *name_dir, int len_dir, char *path)
 {
     DIR *d = opendir(dir);
     if (d == NULL) {
@@ -38,8 +37,8 @@ int traverse(const char *dir, const char *name_dir, int len_dir)
     }
     while ((dd = readdir(d))) {
         if (strcmp(dd->d_name, ".") && strcmp(dd->d_name, "..")) {
-            int slen = snprintf(&path[len_dir], sizeof(path) - len_dir, "/%s", dd->d_name);
-            if (slen + len_dir + 1 <= sizeof(path)) {
+            int slen = snprintf(&path[len_dir], PATH_MAX - len_dir, "/%s", dd->d_name);
+            if (slen + len_dir + 1 <= PATH_MAX) {
                 struct stat info;
                 if (lstat(path, &info) == 0){
                     if (S_ISDIR(info.st_mode)) {
@@ -61,8 +60,8 @@ int traverse(const char *dir, const char *name_dir, int len_dir)
         qsort(v, count, sizeof(*v), compare);
     }
     for (size_t i = 0; i < count; ++i) {
-        int slen = snprintf(&path[len_dir], sizeof(path) - len_dir, "/%s", v[i]);
-        if (traverse(path, v[i], slen + len_dir)){
+        int slen = snprintf(&path[len_dir], PATH_MAX - len_dir, "/%s", v[i]);
+        if (traverse(path, v[i], slen + len_dir, path)){
             return 1;
         }
         free(v[i]);
@@ -76,6 +75,7 @@ int traverse(const char *dir, const char *name_dir, int len_dir)
 
 int main(int argc, char *argv[]) {
     int len = (int) strlen(argv[1]);
+    char path[PATH_MAX];
     sprintf(path, "%s", argv[1]);
-    return traverse(argv[1], NULL, len);
+    return traverse(argv[1], NULL, len, path);
 }
